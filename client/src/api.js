b4d9f41e5d26bd2412152dbf05b487c2c49b1f9a
hilalozturk1 <hlloztrk68@gmail.com>
2023-01-22 12:14:23 +0300

    product detail endpoint

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
 export const fecthProductList = async () => {
-  const {data} = await axios.get("http://localhost:4000/product?page=1");
+  const { data } = await axios.get("http://localhost:4000/product?page=1");
+  return data;
+};
+
+export const fecthProduct = async (product_id) => {
+  const { data } = await axios.get(`http://localhost:4000/product/${product_id}`);
   return data;
 };
