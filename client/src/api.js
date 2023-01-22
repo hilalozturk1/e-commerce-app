4e7fdb47726cc1f31655ef3158edfa3f16a7383a
hilalozturk1 <hlloztrk68@gmail.com>
2023-01-22 12:52:29 +0300

    added environment variable

--- client/src/api.js
+++ client/src/api.js
@@ -1,11 +1,10 @@
 import axios from "axios";
-
 export const fecthProductList = async () => {
-  const { data } = await axios.get("http://localhost:4000/product?page=1");
+  const { data } = await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}/product?page=1`);
   return data;
 };
 
 export const fecthProduct = async (product_id) => {
-  const { data } = await axios.get(`http://localhost:4000/product/${product_id}`);
+  const { data } = await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}/product/${product_id}`);
   return data;
 };
