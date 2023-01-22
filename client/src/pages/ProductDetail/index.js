b3d108596f5fe8264e77b4b8b347fa09ab87d51a
hilalozturk1 <hlloztrk68@gmail.com>
2023-01-22 12:41:38 +0300

    product detail appearance

--- client/src/pages/ProductDetail/index.js
+++ client/src/pages/ProductDetail/index.js
@@ -1,6 +1,9 @@
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import { fecthProduct } from "../../api";
+import { Box, Button, Text } from "@chakra-ui/react";
+import moment from "moment";
+import ImageGallery from "react-image-gallery";
 
 function ProductDetail() {
   const { product_id } = useParams();
@@ -11,7 +14,25 @@ function ProductDetail() {
 
   if (error) return "An error has occurred: " + error.message;
   console.log("data", data);
-  return <div>{product_id}</div>;
+
+  const images = data.photos.map((item) => {
+    return { original: item };
+  });
+
+  console.log(images);
+  return (
+    <div>
+      <Button colorScheme="pink">Add to basket</Button>
+      <Text as="h2" fontSize="2xl">
+        {data.title}
+      </Text>
+      <Text>{moment(data.createdAt).format("DD/MM/YYYY")}</Text>
+      <p>{data.description}</p>
+      <Box margin="10px">
+        <ImageGallery items={images} />
+      </Box>
+    </div>
+  );
 }
 
 export default ProductDetail;
