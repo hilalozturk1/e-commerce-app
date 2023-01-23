a6d4eb334f6d61a0f656ba5b611228c2c95f6bd2
hilalozturk1 <hlloztrk68@gmail.com>
2023-01-23 12:12:02 +0300

    design form elements with chacra

--- client/src/pages/Auth/Signup/index.js
+++ client/src/pages/Auth/Signup/index.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { Flex, Box, Heading, FormLabel, FormControl, Input, Button } from "@chakra-ui/react";
 
 function Signup() {
   return (
-    <div>Signup</div>
-  )
+    <div>
+      <Flex align="center" width="full" justifyContent="center">
+        <Box pt="10">
+          <Box textAlign="center">
+            <Heading>Sing Up</Heading>
+          </Box>
+          <Box my="5" textAlign="left">
+            <form onSubmit={() => {}}>
+              <FormControl>
+                <FormLabel>E-mail</FormLabel>
+                <Input name="email" />
+              </FormControl>
+
+              <FormControl mt="4">
+                <FormLabel>Password</FormLabel>
+                <Input name="password" type="password" />
+              </FormControl>
+
+              <FormControl mt="4">
+                <FormLabel>Password Confirm</FormLabel>
+                <Input name="passwordConfirm" type="password" />
+              </FormControl>
+
+              <Button mt="4" type="submit" width="full">
+                Sing up
+              </Button>
+            </form>
+          </Box>
+        </Box>
+      </Flex>
+    </div>
+  );
 }
 
-export default Signup
+export default Signup;
