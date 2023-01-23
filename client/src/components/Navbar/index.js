ef6dad02e59dcb7df4988b99be5e5735c4325136
hilalozturk1 <hlloztrk68@gmail.com>
2023-01-23 14:38:14 +0300

    changes proper loggedIn

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -2,8 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./styles.module.css";
 import { Button } from "@chakra-ui/react";
+import { useAuth } from "../../context/AuthContext";
 
 function Navbar() {
+  const { loggedIn } = useAuth();
+  console.log(loggedIn);
   return (
     <nav className={styles.nav}>
       <div className={styles.left}>
@@ -12,18 +15,29 @@ function Navbar() {
         </div>
         <ul className={styles.menu}>
           <li>
-            <Link to="/">Products</Link>{/** */}
+            <Link to="/">Products</Link>
           </li>
         </ul>
       </div>
 
       <div className={styles.right}>
-        <Link to="/signin">
-          <Button colorScheme="pink">Login</Button>
-        </Link>
-        <Link to="/signup">
-          <Button colorScheme="pink">Register</Button>
-        </Link>
+        {!loggedIn && (
+          <>
+            <Link to="/signin">
+              <Button colorScheme="pink">Login</Button>
+            </Link>
+            <Link to="/signup">
+              <Button colorScheme="pink">Register</Button>
+            </Link>
+          </>
+        )}
+        {loggedIn && (
+          <>
+            <Link to="/profile">
+              <Button colorScheme="pink">Profile</Button>
+            </Link>
+          </>
+        )}
       </div>
     </nav>
   );
