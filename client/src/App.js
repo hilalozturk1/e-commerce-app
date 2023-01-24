27bb4b3f6b4fdfbe1d018a50a8b506530dc8dd41
hilalozturk1 <hlloztrk68@gmail.com>
2023-01-24 14:24:48 +0300

    error page and routing

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import ProductDetail from "./pages/ProductDetail";
 import Profile from "./pages/Profile";
 import ProtectedRoute from "./pages/ProtectedRoute";
 import Basket from "./pages/Basket";
+import Error from "./pages/Error";
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
               <Route path="/profile" element={<Profile />} />
             </Route>
             <Route path="/basket" element={<Basket />}></Route>
+            <Route path="/*" element={<Error />}></Route>
           </Routes>
         </div>
       </div>
