83478566e69f382877ccc09d0740aa81c3e43f62
hilalozturk1 <hlloztrk68@gmail.com>
2023-01-24 17:51:09 +0300

    admin page

---
+++ client/src/pages/Admin/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+function Admin() {
+  return (
+    <div>
+      <nav>
+        <ul className="admin-menu">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Admin;
