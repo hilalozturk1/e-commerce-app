e185baf9401ee29300300b5d5cdc54f11438935a
hilalozturk1 <hlloztrk68@gmail.com>
2023-01-24 19:38:34 +0300

    fetch order data in orders page

--- client/src/pages/Admin/Orders/index.js
+++ client/src/pages/Admin/Orders/index.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import { useQuery } from "react-query";
+import { fecthOrders } from "../../../api";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from "@chakra-ui/react";
 
 function Orders() {
-  return (
-    <div>Orders</div>
-  )
-}
+  const { isLoading, isError, data, error } = useQuery("admin:orders", fecthOrders);
+  if (isLoading) {
+    return <div>Loading</div>;
+  }
 
-export default Orders
+  if (isError) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  console.log("data: ", data);
+  return (
+    <TableContainer>
+      <Table variant="simple">
+        <TableCaption>Imperial to metric conversion factors</TableCaption>
+        <Thead>
+          <Tr>
+            <Th>User</Th>
+            <Th>Address</Th>
+            <Th isNumeric>Items</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {" "}
+          {data.map((item) => (
+            <Tr key={item._id}>
+              <Td>{item.user.email}</Td>
+              <Td>{item.adress}</Td>
+              <Td isNumeric>{item.items.length}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
+}
+export default Orders;
